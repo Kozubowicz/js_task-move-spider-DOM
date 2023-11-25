14c9d0b5a0fe4ccd37f35960b894cf3921cc5a4b
Kozubowicz <55114447+Kozubowicz@users.noreply.github.com>
2023-11-25 15:22:27 +0100

    make build

---
+++ index.fb391dca.js
@@ -0,0 +1,3 @@
+const e=document.querySelector(".spider");document.addEventListener("click",t=>{if(t.target.classList.contains("wall")){let s=t.offsetX,n=t.offsetY;s>375?s=375:s<25&&(s=25),n>375?n=375:n<25&&(n=25),e.style.marginLeft=s-25+"px",e.style.marginTop=n-25+"px"}});//# sourceMappingURL=index.fb391dca.js.map
+
+//# sourceMappingURL=index.fb391dca.js.map
